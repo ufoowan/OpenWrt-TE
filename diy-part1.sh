10c79450366a9b01d986f9db78dbf5479258e3b2
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-11-11 15:59:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
