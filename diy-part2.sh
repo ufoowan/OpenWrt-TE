f075fee6560e16bed02b52db66f167eddba6993b
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-11-22 11:17:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.9/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/hostname='OpenWrt'/hostname='PBR-M1'/g" package/base-files/files/bin/config_generate
