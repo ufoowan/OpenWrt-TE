01e454130c7784ddf653f9ea976c642837f271f3
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-11-23 08:21:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,8 @@
 #git clone https://github.com/kenzok8/small.git package/small
 svn co https://github.com/kiddin9/openwrt-packages/trunk/gowebdav package/gowebdav
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-diskman package/luci-app-diskman
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-gowebdav package/luci-app-gowebdav
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-syncthing package/luci-app-syncthing
 rm -rf package/lean/luci-theme-argon
@@ -22,7 +24,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.9/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/hostname='OpenWrt'/hostname='PBR-M1'/g" package/base-files/files/bin/config_generate
