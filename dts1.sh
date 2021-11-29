ce1ea98fd06b86287a30dd7cff26fb40486d5e6c
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-11-29 15:33:58 +0800

    Update and rename dts2.sh to dts1.sh

---
+++ dts1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Test
+wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/dts1 -O target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
--- dts2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Test
-wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/dts2 -O target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
