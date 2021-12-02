ab55d9ab4b885f803fe6f6eac02cbf5a624f54c7
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-02 09:26:31 +0800

    Update dts.sh

--- TEST-3/dts.sh
+++ TEST-3/dts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Test
-svn export https://github.com/lc-sub/LCsub-OpenWrt-TE/trunk/TEST-1/dts target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
-svn export https://github.com/lc-sub/LCsub-OpenWrt-TE/trunk/TEST-1/led target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/dts1 -O target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
-#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/led -O target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
+svn export https://github.com/lc-sub/LCsub-OpenWrt-TE/trunk/TEST-3/dts target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
+svn export https://github.com/lc-sub/LCsub-OpenWrt-TE/trunk/TEST-3/led target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
+#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/TEST-3/dts -O target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
+#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/TEST-3/led -O target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
