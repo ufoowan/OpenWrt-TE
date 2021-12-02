cab2cfc44d44f79a83364a1b0ac35636c59de032
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-02 09:37:45 +0800

    Delete dts2.sh

--- dts2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Test
-wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/dts2 -O target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
-#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/net -O target/linux/ramips/mt7621/base-files/etc/board.d/02_network
