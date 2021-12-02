1ed52f912a644f75e3ffb292be2117cd03350031
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-02 09:28:01 +0800

    Update dts.sh

--- TEST-2/dts.sh
+++ TEST-2/dts.sh
@@ -2,3 +2,4 @@
 
 # Test
 svn export https://github.com/lc-sub/LCsub-OpenWrt-TE/trunk/TEST-2/dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
+#wget -nv https://github.com/lc-sub/LCsub-OpenWrt-TE/raw/main/TEST-2/dts -O target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
