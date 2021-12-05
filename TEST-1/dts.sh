6b9913422a7f14be2ab86f9d59118b784a04cff3
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-05 16:36:58 +0800

    Update dts.sh

--- TEST-1/dts.sh
+++ TEST-1/dts.sh
@@ -3,5 +3,4 @@
 # Test
 svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
 svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/led target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-#wget -nv https://github.com/lc-sub/OpenWrt-TE/raw/main/TEST-1/dts -O target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
-#wget -nv https://github.com/lc-sub/OpenWrt-TE/raw/main/TEST-1/led -O target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
+svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/net target/linux/ramips/mt7621/base-files/etc/board.d/02_network
