e6b6febf72c722caaaa39cbbdad41a076b6be9ff
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-08 15:47:06 +0800

    Delete TEST-3 directory

--- TEST-3/dts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Test
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-3/dts target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-3/led target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-3/net target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#wget -nv https://github.com/lc-sub/OpenWrt-TE/raw/main/TEST-3/dts -O target/linux/ramips/dts/mt7621_xzwifi_creativebox-v1.dts
-#wget -nv https://github.com/lc-sub/OpenWrt-TE/raw/main/TEST-3/led -O target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-#wget -nv https://github.com/lc-sub/OpenWrt-TE/raw/main/TEST-3/net -O target/linux/ramips/mt7621/base-files/etc/board.d/02_network
