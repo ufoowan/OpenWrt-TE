1842b5e90c191ff69f22a3504cba88265bcc88e8
lc-sub <92238067+lc-sub@users.noreply.github.com>
2021-12-08 15:47:30 +0800

    Delete TEST-1 directory

--- TEST-1/dts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Test
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/led target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/TEST-1/net target/linux/ramips/mt7621/base-files/etc/board.d/02_network
