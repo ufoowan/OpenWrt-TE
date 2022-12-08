b33b90d6412034d56e64409e55832e9c1cc9ba95
ufoowan <51693545+ufoowan@users.noreply.github.com>
2022-12-08 10:19:12 +0800

    Update p1.sh

--- .patch/p1.sh
+++ .patch/p1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
 # Add a feed source
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
