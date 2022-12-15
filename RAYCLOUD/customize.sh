401b70ade3007e5fd9ec0bf8fd3060169c937ad8
ufoowan <51693545+ufoowan@users.noreply.github.com>
2022-12-15 11:44:50 +0800

    Update customize.sh

--- RAYCLOUD/customize.sh
+++ RAYCLOUD/customize.sh
@@ -13,7 +13,7 @@ sed -i "s/OpenWrt /lc-sub build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" packa
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Device exclusive
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/RAYCLOUD/dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/RAYCLOUD/leds target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/RAYCLOUD/network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-svn export --force https://github.com/lc-sub/OpenWrt-TE/trunk/RAYCLOUD/mk target/linux/ramips/image/mt7621.mk
+svn export --force https://github.com/ufoowan/OpenWrt-TE/trunk/RAYCLOUD/dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
+svn export --force https://github.com/ufoowan/OpenWrt-TE/trunk/RAYCLOUD/leds target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
+svn export --force https://github.com/ufoowan/OpenWrt-TE/trunk/RAYCLOUD/network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+svn export --force https://github.com/ufoowan/OpenWrt-TE/trunk/RAYCLOUD/mk target/linux/ramips/image/mt7621.mk
